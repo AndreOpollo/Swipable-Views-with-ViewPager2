b15a7e723bca9437c1fa7b5ed1c5361100aa9fb6
Andre <opolloandres@gmail.com>
2023-01-12 11:18:31 +0300

    Added the viewpager

--- app/src/main/java/com/example/swipableviews/MainActivity.kt
+++ app/src/main/java/com/example/swipableviews/MainActivity.kt
@@ -3,9 +3,23 @@ package com.example.swipableviews
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+import androidx.viewpager2.widget.ViewPager2
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val images = listOf(
+            R.drawable.health1,
+            R.drawable.hungry1,
+            R.drawable.img2,
+            R.drawable.hungry2,
+            R.drawable.ngo2,
+            R.drawable.savings1
+        )
+      val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        val adapter= ViewPagerAdapter(images)
+        viewPager.adapter = adapter
     }
 }
